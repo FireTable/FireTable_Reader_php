ac1906722737e751db028ec8182f2d94854d6dc0
村村 <727471643@qq.com>
2017-06-25 21:45:30 +0800

    完成书架的相关增删改api
    完成书架的相关增删改api

---
+++ app/Controller/BookController.php
@@ -0,0 +1,67 @@
+<?php
+namespace App\Controller;
+
+use App\Database;
+use App\Model\Book;
+
+class BookController
+{
+
+    public function createBook($request)
+    {
+        $data = $request->getParsedBody();
+        //where多条件查询
+        $checkBook = Book::where([
+          'book_id'=>$data['book_id'],
+          'user_id'=>$data['user_id']
+        ])->first();
+        if($checkBook == null){
+           $Book = new Book;
+           $Book->book_id = $data['book_id'];
+           $Book->book_name = $data['book_name'];
+           $Book->book_icon = $data['book_icon'];
+           $Book->book_page = $data['book_page'];
+           $Book->user_id = $data['user_id'];
+           $Book->save();
+           $Book['state'] = 'success';
+          return json_encode($Book);
+        }else{
+          return json_encode(array('state'=>'fail'));
+        }
+    }
+
+    public function updateBook($request,$response,$args)
+    {
+        $id = $args['id'];
+        $data = $request->getParsedBody();
+        $Book = Book::find($id);
+        $Book->book_page = $data['book_page'];
+        $Book->save();
+        $Book['state'] = 'success';
+        return json_encode($Book);
+    }
+
+    public function deleteBook($request,$response,$args)
+    {
+        $id = $args['id'];
+        $Book = Book::find($id);
+        $Book->delete();
+        return json_encode(array('state'=>'success'));
+    }
+
+
+    public function query($request,$response,$args)
+    {
+        $user_id = $args['user_id'];
+        //where多条件查询
+        $Book = Book::where([
+          'user_id'=>$user_id
+        ])->get();
+        if(sizeof($Book) == 0){
+          $Book = array('state'=>'fail');
+        }else{
+          $Book['state'] = 'success';
+        }
+        return json_encode($Book);
+    }
+}
--- app/routes.php
+++ app/routes.php
@@ -5,11 +5,29 @@ new \App\Database\Capsule;
 
 //获取七牛token
 $app->get('/token', 'App\Controller\UserController:getToken');
-//登录
-$app->get('/user/{username}/{password}', 'App\Controller\UserController:login');
-//注册
-$app->post('/user', 'App\Controller\UserController:createUser');
-//修改
-$app->patch('/user/{id}', 'App\Controller\UserController:updateUser');
+
+//用户群组
+$app->group('/user', function () {
+    //登录
+    $this->get('/{username}/{password}', 'App\Controller\UserController:login');
+    //注册
+    $this->post('', 'App\Controller\UserController:createUser');
+    //修改
+    $this->patch('/{id}', 'App\Controller\UserController:updateUser');
+});
+
+//书架群组
+$app->group('/bookShelf', function () {
+    //查询
+    $this->get('/{user_id}', 'App\Controller\BookController:query');
+    //添加
+    $this->post('', 'App\Controller\BookController:createBook');
+    //修改
+    $this->patch('/{id}', 'App\Controller\BookController:updateBook');
+    //删除
+    $this->delete('/{id}', 'App\Controller\BookController:deleteBook');
+});
+
+
 
 ?>
